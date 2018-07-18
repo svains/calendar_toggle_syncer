a5e284c203ec5c759c56a1204a1dac8315ac2b75
Filip Výborný <svains@gmail.com>
2018-07-18 17:33:02 +0200

    Fix when there are no attendees

--- sync.rb
+++ sync.rb
@@ -22,7 +22,7 @@ else
     next if google_calendar.skip?(event.summary)
 
     # Skip if declined event
-    next if event.attendees.select {|attendee| attendee.email.start_with?(ENV['BB_LOGIN']) && %w(accepted tentative).include?(attendee.response_status)}.empty?
+    next if event.attendees && event.attendees.select {|attendee| attendee.email.start_with?(ENV['BB_LOGIN']) && %w(accepted tentative).include?(attendee.response_status)}.empty?
 
     start_time = event.start.date || event.start.date_time
     end_time   = event.end.date   || event.end.date_time
